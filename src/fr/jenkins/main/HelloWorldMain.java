095edb27472944602c27eb3be9c094b987f9afc9
jeremy <jeremy.langlet@gmail.com>
2021-06-07 22:05:37 +0200

    test jenkins

--- src/fr/jenkins/main/HelloWorldMain.java
+++ src/fr/jenkins/main/HelloWorldMain.java
@@ -3,7 +3,7 @@ package fr.jenkins.main;
 public class HelloWorldMain {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world !!");
+		System.out.println("Hello jeremy !!");
 
 	}
 
