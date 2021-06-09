a2b817f23ea23ea53dcf47dc71954d65a9649f4b
jerem-lan <58588017+jerem-lan@users.noreply.github.com>
2021-06-09 19:31:38 +0200

    Update HelloWorldMain.java

--- src/fr/jenkins/main/HelloWorldMain.java
+++ src/fr/jenkins/main/HelloWorldMain.java
@@ -3,7 +3,7 @@ package fr.jenkins.main;
 public class HelloWorldMain {
 
 	public static void main(String[] args) {
-		System.out.println("Hello jeremy !!");
+		System.out.println("Hello ludi!!");
 
 	}
 
